500f377429e5cdbb355afd2d5b0ee61a8103ffb1
Peter Vanbroekhoven <peter@xaop.com>
2011-08-17 11:12:48 +0200

    Added config option for jquery location

--- lib/delayed_job_admin/engine.rb
+++ lib/delayed_job_admin/engine.rb
@@ -1,4 +1,12 @@
 module DelayedJobAdmin
   class Engine < Rails::Engine
   end
+  
+  def self.jquery_url
+    @jquery_url ||= "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"
+  end
+  
+  def self.jquery_url=(url)
+    @jquery_url = url
+  end
 end
